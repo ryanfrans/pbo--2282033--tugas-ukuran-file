8ef16afeac931bc970e7623492246604f2601546
ryanfrans <2282033@unai.edu>
2026-09-14 14:20:49 +0700

    menambahkan nama file dan ukuran byte

---
+++ src/UkuranFile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UkuranFile {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.int);
+
+        System.out.print("Nama file   : ");
+        String namafile = scanner.nextLine();
+
+        System.out.print("Ukuran (byte) : ");
+        long ukuranByte = scanner.nextLong();
+
+
+
+     }
+}
